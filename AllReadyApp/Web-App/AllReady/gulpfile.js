0170eede9ea9c1662cb8079fc782be7b863202d7
Gary Chan <gary.chan@c0g1t8.com>
2019-02-23 12:22:55 -0800

    Updated to Gulp 4.0.0

--- AllReadyApp/Web-App/AllReady/gulpfile.js
+++ AllReadyApp/Web-App/AllReady/gulpfile.js
@@ -1,4 +1,4 @@
-﻿/// <binding Clean='clean' ProjectOpened='watch' />
+/// <binding Clean='clean' ProjectOpened='watch' />
 var gulp = require("gulp"),
     rimraf = require("rimraf"),
     concat = require("gulp-concat"),
@@ -23,35 +23,37 @@ gulp.task("clean", function (cb) {
     rimraf(paths.concatCssDest, cb);
 });
 
-gulp.task('build:lib', function () {
+gulp.task('build:lib', function (done) {
     function getNPMDependencies() {
         var buffer, packages, keys;
         buffer = fs.readFileSync('package.json');
         packages = JSON.parse(buffer.toString());
         keys = [];
-    
+
         for (var key in packages.dependencies) {
             keys.push(key);
         }
-    
+
         return keys;
     }
 
     function copyNodeModule(name) {
         gulp.src('node_modules/' + name + '/**/*')
             .pipe(gulp.dest('wwwroot/lib/' + name + '/'));
-    };
+    }
 
     var dependencies = getNPMDependencies();
     for (var i in dependencies) {
-        copyNodeModule(dependencies[i])
+        copyNodeModule(dependencies[i]);
     }
-})
+    done();
+});
 
-gulp.task('build:ts', function () {
+gulp.task('build:ts', function (done) {
     var tsResult = tsProject.src()
         .pipe(tsProject())
         .js.pipe(gulp.dest(paths.webroot));
+    done();
 });
 
 gulp.task("build:css", function () {
@@ -61,10 +63,9 @@ gulp.task("build:css", function () {
         .pipe(gulp.dest("."));
 });
 
-gulp.task("build", ["build:lib", "build:ts", "build:css"]);
-gulp.task("min", ["build"]);
+gulp.task("build", gulp.series("build:lib", "build:ts", "build:css"));
+gulp.task("min", gulp.series("build"));
 
 gulp.task("watch", function () {
     gulp.watch([paths.css, paths.ts], ["build"]);
 });
-
